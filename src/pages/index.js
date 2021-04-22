75386aa1374c19fa001acd64c9d49926c38588fb
kaanmertkoc <46743238+kaanmertkoc@users.noreply.github.com>
2021-04-22 17:22:27 +0300

    Initial Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,20 +8,10 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <h1>Learn to design and code React Apps</h1>
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      Complete courses about the best tools and design systems. Prototype and
+      build apps with React and Swift. Now go build something great.
     </p>
   </Layout>
 )
